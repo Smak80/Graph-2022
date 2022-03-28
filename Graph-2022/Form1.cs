0cb273e0c1472a9d2ed27eb06aaa248acc635dc3
Sergey Makletsov <smaklets@kpfu.ru>
2022-03-28 13:32:52 +0300

    GraphPainter

--- Graph-2022/Form1.cs
+++ Graph-2022/Form1.cs
@@ -2,9 +2,20 @@ namespace Graph_2022
 {
     public partial class Form1 : Form
     {
+        private Loader l = new Loader("data.csv");
+        private Graph gr;
+        private GraphPainter gp;
         public Form1()
         {
             InitializeComponent();
+            var d = l.Load();
+            gr = new Graph(d);
+            gp = new GraphPainter(gr);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            gp.Paint(e.Graphics);
         }
     }
 }
---
+++ Graph-2022/GraphPainter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_2022
+{
+    public class GraphPainter
+    {
+        private Graph _graph;
+        private int vertexSize = 30;
+        public GraphPainter(Graph graph)
+        {
+            _graph = graph;
+        }
+
+        public void Paint(Graphics g)
+        {
+            //PaintEdges(g);
+            PaintVertices(g);
+        }
+
+        private void PaintVertices(Graphics g)
+        {
+            var r = Rectangle.Ceiling(g.VisibleClipBounds);
+            g.TranslateTransform(r.Width/2, r.Height/2);
+            g.DrawEllipse(
+                new Pen(Color.BlueViolet, 2), 
+                -vertexSize / 2, 
+                -r.Height / 2 + vertexSize / 2, 
+                vertexSize,
+                vertexSize
+                );
+        }
+    }
+}
