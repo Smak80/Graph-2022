08b2c4e68f21c6074716a5510e6d36c660c6522a
Sergey Makletsov <smaklets@kpfu.ru>
2022-03-28 12:45:17 +0300

    Добавьте файлы проекта.

---
+++ Graph-2022/Form1.cs
@@ -0,0 +1,10 @@
+namespace Graph_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Graph-2022/Loader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_2022
+{
+    public class Loader
+    {
+        private string _filename;
+        public Loader(string filename)
+        {
+            _filename = filename;
+        }
+
+        public double[,]? Load()
+        {
+            StreamReader? sr = null;
+            try
+            {
+                sr = new StreamReader(_filename);
+                double[,]? result = null;
+                int row = 0;
+                while (!sr.EndOfStream)
+                {
+                    var data = sr.ReadLine();
+                    if (data is not null)
+                    {
+                        var vals = data.Split(new []{';',' ','\n','\r'});
+                        result = new double[vals.Length, vals.Length];
+                        var col = 0;
+                        foreach (var val in vals)
+                        {
+                            result[row, col++] = double.Parse(val);
+                        }
+
+                        row++;
+                    }
+                }
+
+                return result;
+            }
+            finally
+            {
+                sr?.Close();
+            }
+        }
+    }
+}
